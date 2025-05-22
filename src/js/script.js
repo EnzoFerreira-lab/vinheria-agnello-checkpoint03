255f91cf7d48bd118dea119262ee0f4d0c882c37
Enzo <enzocafaria@gmail.com>
2025-05-22 20:58:45 -0300

    adicionando arrays de objetos pra representar os vinhos

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+
+const vinhos = [
+    { nome: "Cabernet Sauvignon", tipo: "Tinto", safra: 2018, estoque: 8 },
+    { nome: "Chardonnay", tipo: "Branco", safra: 2020, estoque: 3 },
+    { nome: "Merlot", tipo: "Tinto", safra: 2019, estoque: 12 },
+    { nome: "Rosé Provence", tipo: "Rosé", safra: 2021, estoque: 2 },
+    { nome: "Sauvignon Blanc", tipo: "Branco", safra: 2022, estoque: 5 }
+];
+
